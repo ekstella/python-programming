5a98be5001768fe6dc2c7365cbb459e166fe964e
Ella Ekström <eltsu.ekstrom@gmail.com>
2025-02-26 14:15:22 +0200

    solved functions add

---
+++ T4/functions_add.py
@@ -0,0 +1,10 @@
+def add(first_input, second_input):
+    return int(first_input + second_input + 0.5)
+
+def main():
+    first_input = float(input("Enter a float: "))
+    second_input = float(input("Enter a float: "))
+    result = add(first_input, second_input)
+    print(result)
+
+main()
